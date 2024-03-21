380f96c8e978354a2e52e7a902adbed862a03787
kkh.kim <kkh.kim@lge.com>
2024-03-21 17:23:06 +0900

    patch

---
+++ SeoulParking/src/views/IntroView.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const IntroView = () => {
+  return (
+    <View>
+      <Text>Intro</Text>
+    </View>
+  );
+};
+
+export default IntroView;
+// const styles = StyleSheet.create({
+//     sectionContainer: {
+//       marginTop: 32,
+//       paddingHorizontal: 24,
+//     },
+//     sectionTitle: {
+//       fontSize: 24,
+//       fontWeight: '600',
+//     },
+//     sectionDescription: {
+//       marginTop: 8,
+//       fontSize: 18,
+//       fontWeight: '400',
+//     },
+//     highlight: {
+//       fontWeight: '700',
+//     },
+//   });
